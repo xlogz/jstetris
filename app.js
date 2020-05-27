5ca40d0b6e331ee77f5b3193bdb0a721db1a078d
xlogz <loganxlow@gmail.com>
2020-05-26 19:32:30 -0700

    Added dropping of pieces

--- app.js
+++ app.js
@@ -46,16 +46,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
     console.log(lTetromino);
 
+    let isFarRight = false;
+    let isFarLeft = false;
+
     let currentColumn = 4;
     let currentRow = 0;
 
+    let random = Math.floor(Math.random()*theTetrominoes.length);
+
     let currentBlock = 0;
     let currentRotation = 0
-    let current = theTetrominoes[currentBlock][currentRotation];
+    let current = theTetrominoes[random][currentRotation];
+
+    
 
     function draw(){
         current.forEach(index => {
             squares[currentColumn + index].classList.add('tetromino');
+            console.log('filling piece at: ' + (currentColumn + index));
+            console.log('checking if far right');
+            console.log (currentColumn + index-1);
+            console.log((currentColumn +index -1)%9);
+            console.log('calculating curentcolumn being in right');
+            console.log(((currentColumn + (index-1)) % 9) === 0)
+            if(currentColumn + index === 9 ||
+              currentColumn + index === 19 ||
+              currentColumn + index === 29 ||
+              currentColumn + index === 39 ||
+              currentColumn + index === 49 ||
+              currentColumn + index === 59 ||
+              currentColumn + index === 69 ||
+              currentColumn + index === 79 ||
+              currentColumn + index === 89 ||
+              currentColumn + index === 99){
+                
+              isFarRight = true;
+            }
+            if((currentColumn + index) === 0 || (currentColumn + index) % 10 === 0){
+                isFarLeft = true;
+            }
         })
     }
 
@@ -66,19 +95,41 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     function moveLeft(){
-        if(currentColumn >= 0){
-            clear();
-            currentColumn--;
-            draw();
-        }
+        isFarRight = false;
+            if(isFarLeft){
+                console.log('is far left');
+            }else{
+                clear();
+                currentColumn--;
+                draw();
+            }
+            
+
     }
 
     function moveRight(){
-        if(currentColumn < 10){
+        isFarLeft = false;
+
+        if(isFarRight){
+            console.log('is far right');
+        }else{
             clear();
             currentColumn++;
             draw();
         }
+            
+
+    }
+
+    function dropPiece(){
+            clear();
+            currentColumn = currentColumn + 10;
+            draw();
+    }
+
+    function dropPieceLoop(){
+        dropPiece();
+        setTimeout(dropPieceLoop, 1000)
     }
 
     function rotateBlock(){
@@ -107,7 +158,14 @@ document.addEventListener('DOMContentLoaded', () => {
         if(e.code === "KeyD"){
             moveRight();
         }
+
+        if(e.code === "KeyS"){
+            dropPiece();
+        }
     }
+
+    dropPieceLoop();
+    
 
     draw();
 })
